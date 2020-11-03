b5870291615704e0f02c0213ae567f388da0cace
riskinputra <riski.n.putra@gmail.com>
2020-11-03 19:19:16 +0700

    pagination checkpoint 2

--- packages/front-component/src/components/SEPagination/index.vue
+++ packages/front-component/src/components/SEPagination/index.vue
@@ -9,11 +9,14 @@
           <i class="icon icon-angle-left" />
         </span>
       </li>
-      <li class="active"><span>1</span></li>
-      <li><span>2</span></li>
-      <li><span>3</span></li>
-      <li><span>4</span></li>
-      <li><span>5</span></li>
+      <li
+        v-for="(page) in Number(totalRows)"
+        :key="page"
+        :class="page == currentPage ? 'active' : null"
+        @click="choosePage(page)"
+      >
+        <span>{{ page }}</span>
+      </li>
       <li>
         <span>
           <i class="icon icon-angle-right" />
@@ -34,6 +37,18 @@ export default {
     className: {
       type: String,
       default: null
+    },
+    totalRows: {
+      type: [Number, String],
+      default: 5
+    },
+    perPage: {
+      type: [Number, String],
+      default: 10
+    },
+    currentPage: {
+      type: [Number, String],
+      default: 1
     }
   },
   computed: {
@@ -42,6 +57,11 @@ export default {
         'se-pagination': true,
         [this.className]: this.className !== null
       }
+    }
+  },
+  methods: {
+    choosePage: function (page) {
+      this.$emit('change', page)
     }
   }
 }
