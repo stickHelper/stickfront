c2542f5a018cd0f3d4be8cd922fb665995b5728d
harun-se <webdev@stickearn.com>
2020-11-06 17:33:01 +0700

    update - SEComboBox default, method selected and give active styling

--- packages/front-component/src/components/SEComboBox/index.vue
+++ packages/front-component/src/components/SEComboBox/index.vue
@@ -1,15 +1,21 @@
 <template>
-	<div :class="classes" :options="options">
-		<div class="se-comboBox__header">
-			<span class="se-comboBox__header-text">All Status</span>
+	<div :class="classes">
+		<select class="se-comboBox__select" v-model="selectedValue" style="display: none;">
+			<option value="">---</option>
+			<option v-for="(option, index) in options" :key="index" :value="option">{{ option }}</option>
+		</select>
+		<div class="se-comboBox__header" :class="{ 'se-comboBox__header--active' : selectedValue }">
+			<span class="se-comboBox__header-text se-comboBox__header-text--active" v-if="selectedValue">{{ getSelectedOption() }}</span>
+			<span class="se-comboBox__header-text" v-else>{{ defaultText }}</span>
 			<span class="se-comboBox__header-icon">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"><defs/><path fill="#B4BCD0" d="M9.2.6A.8.8 0 008 .6l-3 3-3-3A.8.8 0 101 1.8l3.5 3.6a.8.8 0 001.2 0l3.6-3.6a.8.8 0 000-1.2z"/></svg>
 			</span>
 		</div>
 		<div class="se-comboBox__body">
-			<div class="se-comboBox__item" v-for="(item, index) in options">{{ item }}</div>
-			<div class="se-comboBox__item" v-if="getItem() === 0">List not defined</div>
+			<div class="se-comboBox__item" v-for="(option, index) in options" @click="getOption(option)">{{ option }}</div>
+			<div class="se-comboBox__item" v-if="getOptionsLength === 0">List not defined</div>
 		</div>
+		
 	</div>
 </template>
 
@@ -21,11 +27,20 @@
 				type: String,
 				default: null
 			},
+			defaultText: {
+				type: String,
+				default: 'All Status'
+			},
 			options: {
 				type: Array,
 				default() {
 		          return []
 		        },
+			}
+		},
+		data() {
+			return {
+				selectedValue: ''
 			}
 		},
 		computed: {
@@ -34,11 +49,17 @@
 					'se-comboBox':true,
 					[this.className]: this.className !== null
 				}
+			},
+			getOptionsLength() {
+				return this.options.length
 			}
 		},
 		methods: {
-			getItem() {
-				return this.options.length
+			getOption(option) {
+				return this.selectedValue = option
+			},
+			getSelectedOption() {
+				return this.selectedValue
 			}
 		}
 	}
