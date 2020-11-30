4ea2d30036379bde60f71adcc8c4c32b39975d77
harun-se <webdev@stickearn.com>
2020-11-30 15:01:19 +0700

    add - wysiwyg component dependencies ckeditor

---
+++ packages/front-component/src/components/SEWysiwyg/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="classes">
+    <ckeditor
+      v-model="editorData"
+      :editor="editor"
+      :config="editorConfig"
+      :disabled="isDisabled"
+    />
+  </div>
+</template>
+
+<script>
+import CKEditor from '@ckeditor/ckeditor5-vue2'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+export default {
+  name: 'SEWysiwyg',
+  components: {
+    ckeditor: CKEditor.component
+  },
+  props: {
+    data: {
+      type: String,
+      default: null
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: this.data,
+      editorDisabled: this.isDisabled,
+      editorConfig: {
+                    // The configuration of the editor.
+      }
+    }
+  },
+  computed: {
+    classes() {
+      return {
+        'se-wysiwyg': true,
+        'se-wysiwyg--disabled': this.isDisabled
+      }
+    }
+  }
+}
+</script>
---
+++ packages/front-component/src/stories/SEWysiwyg.stories.js
@@ -0,0 +1,48 @@
+import SEWysiwyg from '@/components/SEWysiwyg/index.vue'
+
+export default {
+  title: 'Components/Wysiwyg',
+  component: SEWysiwyg
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { SEWysiwyg },
+  template: '<SEWysiwyg />'
+})
+
+export const Default = Template.bind({})
+
+Default.parameters = {
+  docs: {
+    source: {
+      code: '<SEWysiwyg />'
+    }
+  }
+}
+
+export const Disabled = () => ({
+  components: { SEWysiwyg },
+  template: '<SEWysiwyg :isDisabled="true" />'
+})
+
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: '<SEWysiwyg :isDisabled="true" />'
+    }
+  }
+}
+
+export const DummyData = () => ({
+  components: { SEWysiwyg },
+  template: '<SEWysiwyg data=\'<h2>Taj Mahal: A breathtaking ode to love</h2><p>No trip to India is complete without visiting this spectacular monument, counted among the Seven Wonders of the World. Tourists frequently admit that Taj Mahal "simply cannot be described with words". And that’s probably true. The more you try the more speechless you become. Words give only a semblance of truth. The real truth about its beauty is revealed when you adore different shades of “Taj” depending on the time of the day or when you admire the exquisite inlay work in different corners of the façade.</p>\' />'
+})
+
+DummyData.parameters = {
+  docs: {
+    source: {
+      code: '<SEWysiwyg data=\'<h2>Taj Mahal: A breathtaking ode to love</h2><p>No trip to India is complete without visiting this spectacular monument, counted among the Seven Wonders of the World. Tourists frequently admit that Taj Mahal "simply cannot be described with words". And that’s probably true. The more you try the more speechless you become. Words give only a semblance of truth. The real truth about its beauty is revealed when you adore different shades of “Taj” depending on the time of the day or when you admire the exquisite inlay work in different corners of the façade.</p>\' />'
+    }
+  }
+}
