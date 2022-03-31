9e8f26048c4d45d30790ed76aebff2b41626b0b0
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2022-03-31 09:51:10 +0700

    add new props on chart

--- packages/front-component/src/components/SETreeSelect/AsyncSearching.vue
+++ packages/front-component/src/components/SETreeSelect/AsyncSearching.vue
@@ -3,6 +3,7 @@
     :multiple="true"
     :async="true"
     :load-options="loadOptions"
+    :options="options"
   />
 </template>
 
@@ -14,19 +15,33 @@ const simulateAsyncOperation = fn => {
   setTimeout(fn, 2000)
 }
 
+const options = []
+for (let i = 0; i <= 500; i += 1) {
+  options.push({
+    id: `0-${i}`,
+    label: `0-${i}`
+  })
+}
+
 export default {
   components: {
     SETreeSelect
   },
+  data() {
+    return {
+      options
+    }
+  },
   methods: {
     loadOptions({ action, searchQuery, callback }) {
+      console.log({ action, searchQuery, callback })
       if (action === ASYNC_SEARCH) {
         simulateAsyncOperation(() => {
-          const options = [1, 2, 3, 4, 5].map(i => ({
+          const options1 = [1, 2, 3, 4, 5].map(i => ({
             id: `${searchQuery}-${i}`,
             label: `${searchQuery}-${i}`
           }))
-          callback(null, options)
+          callback(null, options1)
         })
       }
     }
